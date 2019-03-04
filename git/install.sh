3ff6f20886a4c9b5730ee8127d2e68acb46a7f5a
sai <sai@mars>
2019-03-04 22:00:44 +0900

    git: add gitconfig install

--- git/install.sh
+++ git/install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 sudo apt install -y git
+
+source $(realpath `dirname $0`/../lib/libs.sh)
+
+aresame ./gitconfig ~/.gitconfig || {
+	ln --backup=t -s $(realpath ./gitconfig) ~/.gitconfig
+}
