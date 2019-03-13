63ee9f4289156d2a670d8dd953651daa950dc3f2
sai <sai@mars>
2019-03-14 06:53:24 +0900

    git: modify the way make symbolic link

--- git/install.sh
+++ git/install.sh
@@ -4,6 +4,4 @@ sudo apt install -y git
 
 source $(realpath `dirname $0`/../lib/libs.sh)
 
-aresame ./gitconfig ~/.gitconfig || {
-	ln --backup=t -s $(realpath ./gitconfig) ~/.gitconfig
-}
+linkifneed ${S}/gitconfig ~/.gitconfig
