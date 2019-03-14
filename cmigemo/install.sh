29001c3f40a49e12db21eb5d4b8c6abb627cc7a6
sai <sai@mars>
2019-03-14 11:26:06 +0900

    cmigemo: install cmigemo

---
+++ cmigemo/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source $(realpath `dirname $0`/../lib/libs.sh)
+
+aptinstallifneed nkf
+
+function install_cmigemo() {
+    rm -rf cmigemo
+    git clone https://github.com/koron/cmigemo
+    cd cmigemo
+    ./configure
+    make gcc
+    make gcc-dict
+    sudo make gcc-install
+}
+
+type cmigemo >& /dev/null || {
+    (cd "$S" && install_cmigemo)
+}
