ae43834ff8ca4c33df7d49e45fbf5d2347765f39
sai <sai@mars>
2019-03-14 11:34:03 +0900

    emacs: modfiy includes lib

--- emacs/install.sh
+++ emacs/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# depends cmigemo
+source "$(realpath `dirname $0`/../lib/libs.sh)"
 
-source $(realpath `dirname $0`/../lib/libs.sh)
+bash "${S}/../cmigemo/install.sh"
 
 aptinstallifneed emacs
 
-linkifneed ${S}/emacs.d ~/.emacs.d
+linkifneed "${S}/emacs.d" ~/.emacs.d
 
 emacs --script ~/.emacs.d/elpa-byte-compile.el
