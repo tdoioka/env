a69529db8894b10c25fbc77272928026826993e0
sai <sai@mars>
2019-03-14 10:00:05 +0900

    lib: modify to quauts to variable references

--- lib/libs.sh
+++ lib/libs.sh
@@ -9,15 +9,15 @@ S="$(realpath `dirname $0`)"
 
 # compaire $1 and $2, same or not.
 function aresame() {
-	if [[ -e $2 ]]; then
-		local lhs=$(realpath $1)
-		local rhs=$(realpath $2)
+	if [[ -e "$2" ]]; then
+		local lhs="$(realpath $1)"
+		local rhs="$(realpath $2)"
 		# same object
-		if [[ $lhs == $rhs ]]; then
+		if [[ "$lhs" == "$rhs" ]]; then
 			return 0
 		fi
 		# same contents
-		cmp -s $rhs $lhs || return 1
+		cmp -s "$rhs" "$lhs" || return 1
 		return 0
 	else
 		return 1
@@ -29,12 +29,11 @@ function aresame() {
 # if $2 and $1 are not same, backup old $1 and make symboliclink.
 # if $2 and $1 are same, do nothing.
 function linkifneed() {
-    aresame $1 $2 || {
-	backupdir $2
-	ln -v --backup=t -nfs $1 $2
+    aresame "$1" "$2" || {
+	backupdir "$2"
+	ln -v --backup=t -nfs "$1" "$2"
     }
 }
-
 
 # backup dirctory $1 with BACKUPOPTION
 BACKUPDIR_OPT="t"
