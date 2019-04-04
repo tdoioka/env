45a502fc326cda7a72e5d2b7af33e1337b264954
sai <sai@mars>
2019-04-04 23:29:57 +0900

    install: add -l option for list up packages

--- install.sh
+++ install.sh
@@ -6,12 +6,19 @@ source "$(realpath `dirname $0`/lib/base.sh)"
 # set up argument functions
 require argment
 setopt all --all, -a -- Run all install script.
+setopt list --list, -l -- Show all package list.
 parseargs "$@"
 
 # set up install functions
 require installation
 set_argument_string INSTALL_SCRIPT [INSTALL_SCRIPT...]
 init_install_env
+
+# show list packages
+$(getopt list) && {
+    echo "${INSTALL_ITEMS[@]}"
+    exit
+}
 
 # if all flag set then run all install scripts.
 $(getopt all) && {
