3a37545ea5926345e7d39ef3d391242a9ca6b34b
sai <sai@mars>
2019-04-10 23:22:24 +0900

    pipenv: hide env name from prompt

--- pipenv/pipenv.sh
+++ pipenv/pipenv.sh
@@ -1,2 +1,8 @@
+# setup pipenv PATH
 export PATH="${PATH}:$(python -m site --user-base)/bin"
+
+# setup pipenv
 eval "$(pipenv --completion)"
+
+# disable virtual-env prompt display
+export VIRTUAL_ENV_DISABLE_PROMPT=1
