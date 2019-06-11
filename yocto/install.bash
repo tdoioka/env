12f82f949439e6c34bc1a7c2616f90c49094d035
sai <sai@titan>
2019-06-12 08:04:42 +0900

    add yocto

---
+++ yocto/install.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+aptinstallifneed build-essential
+aptinstallifneed chrpath
+aptinstallifneed diffstat
+aptinstallifneed gawk
+aptinstallifneed libncurses5-dev
+aptinstallifneed texinfo
