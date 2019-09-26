8c1d50aead702f25ac6087d75ee2e70bd70463e0
sai <sai@titan>
2019-09-27 01:15:08 +0900

    add docker

---
+++ docker/install.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set_install_env
+
+# see also: https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
+type -p docker >& /dev/null || {
+    aptinstallifneed apt-transport-https \
+		     ca-certificates \
+		     curl \
+		     gnupg-agent \
+		     software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    [[ -n "$(uname -a | grep x86_64)" ]] && {
+	sudo add-apt-repository \
+	     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	     $(lsb_release -cs) \
+ 	     stable"
+    }
+    sudo apt update
+
+    aptinstallifneed docker-ce \
+		     docker-ce-cli \
+		     containerd.io
+}
