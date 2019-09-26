96f9949094414d2ce22194809d768080f22a5fe3
sai <sai@titan>
2019-09-27 00:25:00 +0900

    python: Modify to pyenv independent

--- pyenv/install.opt.bash
+++ pyenv/install.opt.bash
@@ -3,6 +3,8 @@
 set_install_env
 install_depend general
 
+# install pyenv
+#................................................................
 gitclone "git://github.com/yyuu/pyenv.git"
 linkifneed "${B}" ~/.pyenv
 
@@ -22,3 +24,22 @@ aptinstallifneed xz-utils
 aptinstallifneed tk-dev
 
 linkifneed "${S}/pyenv.sh" ~/.shrc.d/pyenv.sh
+
+# install python on pyenv
+#................................................................
+aptinstallifneed libffi-dev
+
+source ~/.shrc.d/pyenv.sh
+
+function pyenv_install() {
+    if [[ -z "$(pyenv versions | grep -ow "${1}" 2>/dev/null)" ]]; then
+	(export CFLAGS=-I/usr/include/openssl;
+	 export LDFLAGS=-L/usr/lib;
+	 pyenv install -v "${1}")
+    fi
+}
+
+# pyenv_install 3.7.2
+pyenv_install 3.6.7
+pyenv global 3.6.7
+echo "PYENV: $(pyenv versions | grep -e '^\*')"
--- python/install.bash
+++ python/install.bash
@@ -2,23 +2,7 @@
 
 set_install_env
 
-install_depend pyenv
-
-aptinstallifneed zlib1g-dev
-aptinstallifneed libbz2-dev
-aptinstallifneed libreadline-dev
-aptinstallifneed libffi-dev
-
-source ~/.shrc.d/pyenv.sh
-
-function pyenv_install() {
-    if [[ -z "$(pyenv versions | grep -ow "${1}" 2>/dev/null)" ]]; then
-	(export CFLAGS=-I/usr/include/openssl;
-	 export LDFLAGS=-L/usr/lib;
-	 pyenv install -v "${1}")
-    fi
-}
-# pyenv_install 3.7.2
-pyenv_install 3.6.7
-pyenv global 3.6.7
-echo "PYENV: $(pyenv versions | grep -e '^\*')"
+aptinstallifneed python
+aptinstallifneed python-pip
+aptinstallifneed python3
+aptinstallifneed python3-pip
