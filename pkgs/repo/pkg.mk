63e63897afbf83b69b01953aadabf202ee28df8a
tdoioka <tdoioka@users.noreply.github.com>
2020-12-22 00:15:04 +0900

    repo: Add repo

---
+++ pkgs/repo/pkg.mk
@@ -0,0 +1,25 @@
+pkg-bdeps := git python
+pkg-rdeps :=
+$(eval $(call pkg-rule-task))
+
+$(pkg-preup):
+	$(log-pre)
+	$(log-post)
+
+$(pkg-update):
+	$(log-pre)
+	$(call pkg-schedule,			\
+		curl				\
+		ca-certificates			\
+		)
+	$(log-post)
+
+$(pkg-all):
+	$(log-pre)
+	$(MAKE) $(.repo_bin)
+	$(log-post)
+
+.repo_bin := $(HOME)/bin/repo
+$(.repo_bin): $(DIRS)
+	curl -fsSL https://storage.googleapis.com/git-repo-downloads/repo -o $@
+	chmod a+x $@
