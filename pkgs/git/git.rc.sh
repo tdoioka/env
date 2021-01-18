100d5da270cec5975838660c66ebd79e8709d53d
tdoioka <tdoioka@users.noreply.github.com>
2021-01-18 14:08:42 +0900

    git: Add a generate gitignore_global from *.gitignore files

--- pkgs/git/git.rc.sh
+++ pkgs/git/git.rc.sh
@@ -1,1 +1,11 @@
 alias g=git
+
+__git::main() {
+  # Download gitignore file from below:
+  # Download from https://github.com/github/gitignore/tree/master/Global
+  # Place at ~/.gitconfig.d with the .gitignore suffix.
+  if ls ~/.gitconfig.d/*.gitignore 2>&1 >/dev/null; then
+    cat ~/.gitconfig.d/*.gitignore > ~/.gitconfig.d/gitignore_global
+  fi
+}
+__git::main
