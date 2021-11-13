8314bcd566fb0b34c167cf80f3a3945fbaa96ae5
tdoioka <tdoioka@users.noreply.github.com>
2021-11-13 23:08:54 +0900

    wsl: Add wsl-vagrant environment settings

--- pkgs/wslcfg/wslcfg.rc.sh
+++ pkgs/wslcfg/wslcfg.rc.sh
@@ -3,6 +3,11 @@
 
 if [ -e /proc/sys/fs/binfmt_misc/WSLInterop ]; then
   addapth "/mnt/c/Program\ Files/Oracle/VirtualBox/"
+  addpath "/mnt/c/Windows/System32/"
+  addpath "/mnt/c/Windows/System32/WindowsPowerShell/v1.0/"
+  export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
+  export VAGRANT_WSL_WINDOWS_ACCESS_USER_HOME_PATH="/mnt/d/virtualbox"
+  export VAGRANT_WSL_WINDOWS_ACCESS_USER="$(cmd.exe /C echo %username% 2>/dev/null)"
   type -p vboxmanage || {
     alias vboxmange='VBoxManage.exe'
   }
